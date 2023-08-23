6aba9b73dc6b6a9454f3f05fd0e22762913cd7b4
tee8z <tee8z@protonmail.com>
2023-08-22 23:48:22 -0400

    Add bash examples (#12)
    * adds examples of how to uses the container aliases in bash and do some interesting lightning things, namely fee bumping and hold invoices.

---
+++ bash_examples/fee_bumping.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ../scripts/container_aliases.sh
+
+#pubkey open channel to
+customer getinfo | jq -r 'identity_pubkey')
+
+#funding_tx
+router1 openchannel --node_key $pubkey --local_amt 900000 | jq -r 'funding_txid')
+
+# repeate the following 2 step to create as many CPFP fee bumps as you'd like
+
+#the outpoint is either 1 or 2
+router1 wallet bumpfee --force --sat_per_vbyte 1000 $fundind_tx:(1|0)
+
+#get list of chain txns sort by created time
+# grab the tx_hash of the sweep if you want to make chain
+# grab the tx_hash of the orignial open to make a new branch of txs
+router1 listchaintxns | jq '.transactions | sort_by(.time_stamp | tonumber)'
+
+#generate at least one block to have a transaction chain for the channel picked
+#mine more blocks if you want to see the channel also confirmed
+#after 1 block, fee bumping doesn't matter (it's a mempool concern)
+bd1 -generate 1
