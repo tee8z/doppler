c81032c9aff6b58c186e05e7cb070ebc23e0950a
tee8z <tee8z@protonmail.com>
2025-01-05 14:56:37 -0500

    revert path change (#106)

--- doppler_ui/src/routes/api/download/+server.ts
+++ doppler_ui/src/routes/api/download/+server.ts
@@ -3,9 +3,8 @@ import type { RequestHandler } from './$types';
 import fs from 'fs';
 import path from 'path';
 import { parse } from 'ini';
-import { UI_CONFIG_PATH } from '$env/static/private';
 
-const configPath = UI_CONFIG_PATH || process.env.UI_CONFIG_PATH || path.join(process.cwd(), '/build/ui_config');
+const configPath = process.env.UI_CONFIG_PATH || path.join(process.cwd(), '/build/ui_config');
 
 const config = parse(fs.readFileSync(`${configPath}/server.conf.ini`, 'utf-8'));
 
